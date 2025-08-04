e4fd7ccf7ab2da3976fb5e4c080bcea59e0bf1e0
Ron Mordukhovich <ronmordo1@outlook.com>
2025-08-04 14:59:41 +0300

    test ok removed comment.

--- backend/src/api/recipes/recipe.schema.ts
+++ backend/src/api/recipes/recipe.schema.ts
@@ -14,7 +14,6 @@ const createRecipeBody = z.object({
     z.literal("meat-based"),
     z.literal("gluten-free"),
   ]),
-  //test
   preparationTime: z.union([
     z.literal(1),
     z.literal(2),
